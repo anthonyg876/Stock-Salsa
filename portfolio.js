8f4732ec5e317763f156d9648388fb86423904d8
Jesse Maki <87655161+jessexmaki@users.noreply.github.com>
2022-11-21 13:25:12 -0500

    Added JS file for User Portfolio

---
+++ portfolio.js
@@ -0,0 +1,47 @@
+"https://jsonplaceholder.typicode.com/users"
+
+const selected = document.querySelector(".selectedContainer");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+    });
+});
+
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    users.forEach(user => {
+        const isVisible =
+            user.name.toLowerCase().includes(value) ||
+            user.email.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisible)
+    })
+})
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+        users = data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0]
+            const header = card.querySelector("[data-header]")
+            const body = card.querySelector("[data-body]")
+            header.textContent = user.name
+            body.textContent = user.email
+            userCardContainer.append(card)
+            return { name: user.name, email: user.email, element: card }
+        })
+    })
