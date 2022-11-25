18254da49216547444d6174252fb709e0d4bcb71
woodsnip <woodsnib@ufl.edu>
2022-11-25 18:26:11 -0500

    more testing

--- chartpage.js
+++ chartpage.js
@@ -33,3 +33,36 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+document.querySelector("#searchButton").addEventListener("click", e => {
+    e.preventDefault();
+    var id = document.querySelector("#searchName");
+    fetch("http://localhost:8080/api/v1/stockPrices/getPrices/" + id, {
+        method: "GET"
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        let i = 0;
+            while (i < data.length) {
+            var cleanData = [];
+            const date = Date.parse(data[i].dateOfPrice);
+            const obj = {
+                "dateOfPrice": date,
+                "open": data[i].open,
+                "high": data[i].high,
+                "low": data[i].low,
+                "adjClose": data[i].adjClosed,
+                "volume": data[i].volume
+            };
+            cleanData.push(obj);
+            console.log(cleanData);
+            i++;
+    }
+    //make AMCHART
+
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
