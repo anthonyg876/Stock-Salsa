4208519a97cb5124c9559f8ebed0ed4ba7d3ef64
= <agravier@ufl.edu>
2022-11-25 18:55:57 -0500

    Added functionality to add the data to the js using the go button.

--- chartpage.js
+++ chartpage.js
@@ -34,35 +34,38 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+function getPrices() {
+    var cleanData = [];
 
-document.querySelector("#searchButton").addEventListener("click", e => {
-    e.preventDefault();
+// document.querySelector("#searchButton").addEventListener("click", e => {
     var id = document.querySelector("#searchName");
-    fetch("http://localhost:8080/api/v1/stockPrices/getPrices/" + id, {
+    console.log(id);
+    var request = "http://localhost:8080/api/v1/stockPrices/getPrices/" + id.value;
+    console.log(request);
+    fetch(request, {
         method: "GET"
     })
     .then((response) => response.json())
     .then((data) => {
         let i = 0;
             while (i < data.length) {
-            var cleanData = [];
-            const date = Date.parse(data[i].dateOfPrice);
-            const obj = {
-                "dateOfPrice": date,
-                "open": data[i].open,
-                "high": data[i].high,
-                "low": data[i].low,
-                "adjClose": data[i].adjClosed,
-                "volume": data[i].volume
-            };
-            cleanData.push(obj);
-            console.log(cleanData);
-            i++;
-    }
+                const date = Date.parse(data[i].dateOfPrice);
+                const obj = {
+                    "dateOfPrice": date,
+                    "open": data[i].open,
+                    "high": data[i].high,
+                    "low": data[i].low,
+                    "adjClose": data[i].adjClosed,
+                    "volume": data[i].volume
+                };
+                cleanData.push(obj);
+                i++;
+            }
+        console.log(cleanData);
     //make AMCHART
 
     })
     .catch((err) => {
         console.log(err);
     });
-});
+};
