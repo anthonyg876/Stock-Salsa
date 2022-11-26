602025b6f3414fee160fb7e58c61dca05fc264ac
= <agravier@ufl.edu>
2022-11-25 19:30:19 -0500

    Fixed requests and now work for collecting data.

--- chartpage.js
+++ chartpage.js
@@ -3,34 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     stockSearch.addEventListener("submit", e => {
         e.preventDefault();
-        var id = document.querySelector("#searchName");
-        fetch("http://localhost:8080/api/v1/stockPrices/getPrices/" + id, {
-            method: "GET"
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            let i = 0;
-                while (i < data.length) {
-                var cleanData = [];
-                const date = Date.parse(data[i].dateOfPrice);
-                const obj = {
-                    "dateOfPrice": date,
-                    "open": data[i].open,
-                    "high": data[i].high,
-                    "low": data[i].low,
-                    "adjClose": data[i].adjClosed,
-                    "volume": data[i].volume
-                };
-                cleanData.push(obj);
-                console.log(cleanData);
-                i++;
-        }
-        //make AMCHART
-
-        })
-        .catch((err) => {
-            console.log(err);
-        });
     });
 });
 
@@ -39,7 +11,7 @@ function getPrices() {
 
 // document.querySelector("#searchButton").addEventListener("click", e => {
     var id = document.querySelector("#searchName");
-    console.log(id);
+    console.log(id.value);
     var request = "http://localhost:8080/api/v1/stockPrices/getPrices/" + id.value;
     console.log(request);
     fetch(request, {
