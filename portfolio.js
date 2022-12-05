4a4046cb7c70ec3d2d12522e98d751c987eb0ebe
woodsnip <woodsnib@ufl.edu>
2022-12-05 08:47:28 -0500

    average volume and average close works

--- portfolio.js
+++ portfolio.js
@@ -5,6 +5,118 @@ const optionsContainer = document.querySelector(".options-container");
 
 const optionsList = document.querySelectorAll(".option");
 
+function dateConverter(date) {
+    let year = "";
+    let month = "";
+    let day = "";
+    for (let i = 0; i < date.length; ++i) {
+        if (i < 4) {
+            year += date[i];
+        }
+        if (i > 4 && i < 7) {
+            month += date[i];
+        }
+        if (i > 7 && i < 10) {
+            day += date[i];
+        }
+    }
+    switch (month) {
+        case "01":
+            month = "JAN";
+            break;
+        case "02":
+            month = "FEB";
+            break;
+        case "03":
+            month = "MAR";
+            break;
+        case "04":
+            month = "APR";
+            break;
+        case "05":
+            month = "MAY";
+            break;
+        case "06":
+            month = "JUN";
+            break;
+        case "07":
+            month = "JUL";
+            break;
+        case "08":
+            month = "AUG";
+            break;
+        case "09":
+            month = "SEP";
+            break;
+        case "10":
+            month = "OCT";
+            break;
+        case "11":
+            month = "NOV";
+            break;
+        case "12":
+            month = "DEC";
+            break;
+        default:
+            month = "JAN";
+            break;
+    }
+    let resultDate = "" + day + "-" + month + "-" + year;
+    return resultDate;
+}
+
+function getAvgVolume() {
+    const messageElement = document.getElementById("message");
+    const form = {
+        Symbol: document.querySelector("#searchName").value,
+        BeginDate: dateConverter(document.querySelector("#StartDate").value),
+        EndDate: dateConverter(document.querySelector("#EndDate").value)
+    };
+    console.log(form);
+    fetch("http://localhost:8080/api/v1/stockPrices/averageVolume", {
+            method: "POST",
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify([form.Symbol, form.BeginDate, form.EndDate]),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            messageElement.innerHTML = "Average Volume of " + form.Symbol + ": " + data;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
+function getAvgClose() {
+    const messageElement = document.getElementById("message");
+    const form = {
+        Symbol: document.querySelector("#searchName").value,
+        BeginDate: dateConverter(document.querySelector("#StartDate").value),
+        EndDate: dateConverter(document.querySelector("#EndDate").value)
+    };
+    console.log(form);
+    fetch("http://localhost:8080/api/v1/stockPrices/averageClose", {
+            method: "POST",
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify([form.Symbol, form.BeginDate, form.EndDate]),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            messageElement.innerHTML = "Average Closing Price of " + form.Symbol + ": " + data;
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+};
+
 selected.addEventListener("click", () => {
     optionsContainer.classList.toggle("active");
 });
