20fdf66e8c3a20d1340fdf02bad844899d3153b5
woodsnip <woodsnib@ufl.edu>
2022-12-05 15:12:29 -0500

    added bottom 5 losers

--- menu.js
+++ menu.js
@@ -138,78 +138,13 @@ function top5DJI() {
         });
 };
 
-function top5DJGT() {
+function losersDJI() {
     const form = {
         BeginDate: dateConverter(document.querySelector("#StartDate").value),
         EndDate: dateConverter(document.querySelector("#EndDate").value),
-        Index: "DJGT"
     }
     console.log(form);
-    fetch("http://localhost:8080/api/v1/stockPrices/highestGrowingStocks", {
-            method: "POST",
-            headers: {
-                Accept: "application/json, text/plain, */*",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify([form.BeginDate, form.EndDate, form.Index]),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            let sortedData = [];
-            for(let i = 0; i < data.length; ++i) {
-                sortedData.push({
-                    Name: data[i].nameOfStock,
-                    Growth: data[i].percentIncrease
-                })
-            }
-            console.log(sortedData);
-            const tbl = document.createElement("table");
-            tbl.id = "tableOne";
-            document.body.appendChild(tbl);
-            const tblBody = document.createElement("tbody");
-
-            // creating all cells
-            for (let i = 0; i < data.length; i++) {
-                // creates a table row
-                const row = document.createElement("tr");
-
-                // Create a <td> element and a text node, make the text
-                // node the contents of the <td>, and put the <td> at
-                // the end of the table row
-                const cell = document.createElement("td");
-                const cellText = document.createTextNode(sortedData[i].Name);
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                const cell2 = document.createElement("td");
-                const cell2Text = document.createTextNode(sortedData[i].Growth);
-                cell2.appendChild(cell2Text);
-                row.appendChild(cell2)
-
-                // add the row to the end of the table body
-                tblBody.appendChild(row);
-            }
-
-            // put the <tbody> in the <table>
-            tbl.appendChild(tblBody);
-            // appends <table> into <body>
-            document.body.appendChild(tbl);
-            // sets the border attribute of tbl to '2'
-            tbl.setAttribute("border", "2");
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-};
-
-function top5NDX() {
-    const form = {
-        BeginDate: dateConverter(document.querySelector("#StartDate").value),
-        EndDate: dateConverter(document.querySelector("#EndDate").value),
-        Index: "NDX"
-    }
-    console.log(form);
-    fetch("http://localhost:8080/api/v1/stockPrices/highestGrowingStocks", {
+    fetch("http://localhost:8080/api/v1/stockPrices/lowestGrowingStocks", {
             method: "POST",
             headers: {
                 Accept: "application/json, text/plain, */*",
